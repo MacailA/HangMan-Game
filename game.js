c84f8aa572fcb982f8555bc389ef3d1470ee67cc
Macail Allen <macail@Macails-MacBook-Air.local>
2019-04-12 11:00:01 -0400

    js for loop for underscores that match  secretwords

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 
-Let words = [
+// Array of secret words for the Hangman game.
+let secretWords = [
 "important",
 "dragon",
 "noah",
@@ -12,4 +13,15 @@ Let words = [
 "playstation"
 ];
 
-let word = words[Math.floor(Math.random() * words.length)];
+// Randomly pick a word from the "secretWords" array.
+let word = secretWords[Math.floor(Math.random() * secretWords.length)];
+
+// I want to create another array with underscores. This will match the number of letters in my secretwords when chose randomly.
+
+let answerArray = [];
+ufor (let i = 0; i < word.length; i++) {
+     answerArray[i] = "_";
+   }
+   let remainingLetters = word.length;
+
+
