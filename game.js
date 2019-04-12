9ebd0b2a48641a411fd442938879e9a6bb6fcc45
Macail Allen <macail@Macails-MacBook-Air.local>
2019-04-12 11:30:57 -0400

    js getting player's guess input

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 
 // Array of secret words for the Hangman game.
 let secretWords = [
-"important",
-"dragon",
-"noah",
-"stanly",
-"supriya",
-"jordan",
-"stickman",
-"apple",
-"apex",
-"playstation"
+    "important",
+    "dragon",
+    "noah",
+    "stanly",
+    "supriya",
+    "jordan",
+    "stickman",
+    "apple",
+    "apex",
+    "playstation"
 ];
 
 // Randomly pick a word from the "secretWords" array.
@@ -19,9 +19,24 @@ let word = secretWords[Math.floor(Math.random() * secretWords.length)];
 // I want to create another array with underscores. This will match the number of letters in my secretwords when chose randomly.
 
 let answerArray = [];
-ufor (let i = 0; i < word.length; i++) {
-     answerArray[i] = "_";
-   }
-   let remainingLetters = word.length;
+for (let i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+let remainingLetters = word.length;
+
+// creating alert to show the correct letters in the word the player guessed.
+alert(answerArray.join(" "));
+
+//Getting the Player's letter guess (input) and update the game state with the player's guess   
+let playersGuess = prompt("Guess a letter, or click Cancel to stop playing.");
+if (guess === null) {
+    break;
+} else if (guess.length !== 1) {
+    alert("Please enter a single letter.");
+} else {
+    // Update the game state with the guess }
+
+
+
 
 
